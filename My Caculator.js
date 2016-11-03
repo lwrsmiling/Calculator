364604a3fbf2adc9ce01e89840dab756e6d068a9
lwrsmiling <wanru@ualberta.ca>
2016-11-02 22:40:26 -0600

    越来越混乱了。。。20161102

--- My Caculator.js
+++ My Caculator.js
@@ -1,45 +1,70 @@
 var input = document.getElementById("input");
 var result = document.getElementById("result");
-var numClicked, numBeforeOper, operStream;
+var numClicked, numBeforeOper, operClicked, stream;
+var operPressed, equalPressed;
 
-var numBtns = document.getElementsByClassName("number");
-for(i=0; i<numBtns.length; i++){
-  
-  numBtns[i].onclick = function(){  
-    numClicked = this.id;
-    input.innerHTML += numClicked;
-
-  };
+/*assign clear-clicked event*/
+var clearBtn = document.getElementById("clear");
+clearBtn.onclick = function(){
+  input.innerHTML = "";
+  result.innerHTML = "";
 }
 
+/*assign number-clicked events*/
+var numBtns = document.getElementsByClassName("number");
+
+for(i=0; i<numBtns.length; i++){
+  numBtns[i].onclick = function (){
+    if(equalPressed || operPressed){
+      input.innerHTML = "";
+      numClicked = this.id;
+      input.innerHTML += numClicked;
+      result.innerHTML += numClicked;
+      stream = parseFloat(input.innerHTML);
+    }
+    else{
+      numClicked = this.id;
+      input.innerHTML += numClicked;
+      result.innerHTML += numClicked;
+      stream = parseFloat(input.innerHTML);
+    }
+  }
+}
+
+
+/*assign operator-clicked events*/
 var operBtns = document.getElementsByClassName("operator");
 for(j=0; j<operBtns.length; j++){
-  operBtns[j].onclick = function(){
-    var operClicked = this.id;
-    operStream = operClicked;
-    numBeforeOper = numClicked;
-    input.innerHTML += operClicked;
-
-  };
+  operBtns[j].onclick = function (){
+    operClicked = this.id;
+    numBeforeOper = stream;
+    input.innerHTML = operClicked;
+    result.innerHTML += input.innerHTML;
+    operPressed = "true";
+  }
 }
 
+
+
+/*assign =clicked events*/
 var equal = document.getElementById("=");
 equal.onclick = function(){
-  switch(operStream){   
+  switch(operClicked){   
   case '+':   
-  result.innerHTML = numBeforeOper+numClicked;   
+  result.innerHTML = numBeforeOper+stream;   
   break;   
   case '-':   
-  result.innerHTML = numBeforeOper-numClicked;   
+  result.innerHTML = numBeforeOper-stream;   
   break;   
   case '*':   
-  result.innerHTML = numBeforeOper*numClicked;   
+  result.innerHTML = numBeforeOper*stream;   
   break;   
   case '/':   
-  result.innerHTML = numBeforeOper/numClicked;  
+  result.innerHTML = numBeforeOper/stream;  
   break;   
   case '%':   
-  result.innerHTML = numBeforeOper%numClicked;
+  result.innerHTML = numBeforeOper%stream;
   break;   
-  }   
+  } 
+  equalPressed = true;   
 }
